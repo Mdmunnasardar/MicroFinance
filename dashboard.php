a685d7f95e680bb4a29fbc44439d2b1cb7ce06ba
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2026-06-08 22:49:40 +0600

    Add dashboard page with user session management and welcome message

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<h1>Welcome <?php echo $_SESSION['name']; ?></h1>
+
+<p>Role: <?php echo $_SESSION['role']; ?></p>
+
+<a href="logout.php">Logout</a>
