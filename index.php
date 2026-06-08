8164b9f4b49e7a0e94dd771a5f18674c7c3889c7
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2026-06-08 22:48:46 +0600

    Implement login functionality with user authentication

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
+session_start();
 include "config/db.php";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MicroFinance Login</title>
+</head>
+<body>
+
+<h2>Login System</h2>
+
+<form method="POST" action="login.php">
+    <input type="text" name="username" placeholder="Username" required><br><br>
+
+    <input type="password" name="password" placeholder="Password" required><br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
