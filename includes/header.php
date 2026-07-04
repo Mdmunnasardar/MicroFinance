abc90b6c0ec8efa7eb8ff3c9d83ebc3b946f94aa
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2026-07-04 18:22:49 +0600

    fix: update comment for committees.css link in header.php for clarity

--- includes/header.php
+++ includes/header.php
@@ -29,7 +29,7 @@ if (!isset($_SESSION['user_id'])) {
     
     <!-- Dashboard CSS -->
     <link rel="stylesheet" href="assets/css/dashboard.css">
-    
+    <!-- Committees CSS -->
      <link rel="stylesheet" href="../assets/css/committees.css">
    
 </head>
